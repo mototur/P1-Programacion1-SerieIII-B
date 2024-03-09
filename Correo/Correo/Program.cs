445bc6a614a8a30ca18fc6082bbfce50d449b2a3
Lucia Sandoval <asandovalp9@miumg.edu.gt>
2024-03-09 13:06:41 -0600

    Primer Parcial
    Serie 2 y Serie 3

---
+++ Correo/Correo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su dirreccion de correo");
+            string correo = Console.ReadLine();
+
+            string nombre = correo.Substring(0, correo.IndexOf("@"));
+            string dominioActual = correo.Substring(correo.IndexOf("@") + 1);
+
+            Console.WriteLine("Introduzca el dominio nuevo: ");
+            string DominioNuevo = Console.ReadLine();
+
+            string CorreoNuevo = nombre + "@" + DominioNuevo;
+
+            Console.WriteLine("Su dirreccion de correo se ha cambiado es: " + CorreoNuevo);
+        }
+    }
+}
---
+++ Serie III, parcial 1/Serie III, parcial 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_III__parcial_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenida");
+            Console.WriteLine("Ingrese primer precio");
+            int precioUno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo precio");
+            int precioDos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer precio");
+            int precioTres = int.Parse(Console.ReadLine());
+            Calculo(precioUno, precioDos, precioTres);
+            Console.ReadKey(true);
+        }
+        public static void Calculo (int precioUno, int precioDos, int precioTres)
+        {
+
+        }
+    }
+}
